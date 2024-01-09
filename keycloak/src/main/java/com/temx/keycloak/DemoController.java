eea0c889741fd32702467b24ee983cd8a3555282
David <osodavid001@gmail.com>
2024-01-09 04:29:05 +0100

    created jwt auth converter

--- keycloak/src/main/java/com/temx/keycloak/DemoController.java
+++ keycloak/src/main/java/com/temx/keycloak/DemoController.java
@@ -1,5 +1,6 @@
 package com.temx.keycloak;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class DemoController {
     @GetMapping
+    @PreAuthorize("hasRole('client_user')")
     public String hello(){
         return "Hello from Spring boot & Keycloak";
     }
 
     @GetMapping("/hello-2")
+    @PreAuthorize("hasRole('client_admin')")
     public String hello2(){
         return "Hello from Spring boot & Keycloak - ADMIN";
     }
--- keycloak/src/main/java/com/temx/keycloak/SecurityConfig.java
+++ keycloak/src/main/java/com/temx/keycloak/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.temx.keycloak;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,7 +12,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthConverter jwtAuthConverter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -20,7 +26,8 @@ public class SecurityConfig {
                                 .authenticated());
         http
                 .oauth2ResourceServer(resourceServer -> resourceServer
-                        .jwt());
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthConverter)));
 
         http
                 .sessionManagement(sessionManagement ->
